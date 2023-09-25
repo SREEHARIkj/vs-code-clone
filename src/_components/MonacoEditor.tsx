7b45157c8b13caeb3d16ff0cddce8a4bbfce4352
SREEHARIkj <sreeharikj8@gmail.com>
2023-09-25 23:51:38 +0530

    monaco- multi editor support

--- src/_components/MonacoEditor.tsx
+++ src/_components/MonacoEditor.tsx
@@ -1,10 +1,33 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 
 import Editor from "@monaco-editor/react";
+import { Button } from "@/components/ui/button";
 
 export default function MonacoEditor() {
+  const [fileName, setFileName] = useState<string>("script.js");
+
+  const files = {
+    "script.js": {
+      name: "script.js",
+      language: "javascript",
+      value: "const val= 0",
+    },
+    "style.css": {
+      name: "style.css",
+      language: "css",
+      value: ".test {color:blue}",
+    },
+    "index.html": {
+      name: "index.html",
+      language: "html",
+      value: "<html>tets</html>",
+    },
+  };
+
+  const file: any = files[fileName];
+
   function handleEditorChange(value, event) {
     // here is the current value
   }
@@ -24,15 +47,36 @@ export default function MonacoEditor() {
   }
 
   return (
-    <Editor
-      height="100%"
-      defaultLanguage="javascript"
-      defaultValue="// some comment"
-      onChange={handleEditorChange}
-      onMount={handleEditorDidMount}
-      beforeMount={handleEditorWillMount}
-      onValidate={handleEditorValidation}
-      theme="vs-dark"
-    />
+    <>
+      <Button
+        disabled={fileName === "script.js"}
+        onClick={() => setFileName("script.js")}
+      >
+        script.js
+      </Button>
+      <Button
+        disabled={fileName === "style.css"}
+        onClick={() => setFileName("style.css")}
+      >
+        style.css
+      </Button>
+      <Button
+        disabled={fileName === "index.html"}
+        onClick={() => setFileName("index.html")}
+      >
+        index.html
+      </Button>
+      <Editor
+        height="100%"
+        theme="vs-dark"
+        path={file.name}
+        defaultLanguage={file.language}
+        defaultValue={file.value}
+        onChange={handleEditorChange}
+        onMount={handleEditorDidMount}
+        beforeMount={handleEditorWillMount}
+        onValidate={handleEditorValidation}
+      />
+    </>
   );
 }
